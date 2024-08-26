711c3761e38b9661d8029ef092effeebc17ecbc0
akhdanrgya <akhdan.anargya@gmail.com>
2024-08-26 16:36:28 +0700

    FIRST CUY

---
+++ app/(screens)/mainApp.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App bang!!!</Text>
+    </View>
+  )
+}
+
+const Styles = StyleSheet.create({
+  container : {
+    backgroundColor : "white",
+    margin : 0,
+    padding : 0
+  }
+})
+
+export default App
